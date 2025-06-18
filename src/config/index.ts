3154bb37681f303270b877a2b35e3023838cb4c7
Nundini <nundinir@umich.edu>
2025-06-18 19:27:46 -0400

    added projects and experiences to main page. needs further workshopping

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,22 +2,21 @@ import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
   title: "Nundini Rawal",
-  author: "Alejandro Múnez Cuntez",
-  description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+  author: "Nundini Rawal",
+  description: `Howdy! I'm a mechanical engineer and scientist at the University of Michigan.
+    I specialize in biomechanics, human-robot interaction, exoskeleton control, and experimental design.`,
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/nundinir-small.jpg",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/nundinir/" },
+    { text: "Github", href: "https://github.com/nundinir" },
+    { text: "ORCID", href: "https://orcid.org/0009-0002-0591-8542" },
+    { text: "Google Scholar", href: "https://scholar.google.com/citations?user=-YWyaAQAAAAJ&hl=en&oi=ao" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -26,70 +25,88 @@ export const SITE_CONFIG: SiteConfig = {
 export const SITE_CONTENT: SiteContent = {
   hero: {
     name: "Nundini Rawal",
-    specialty: "Mobile & Web Developer",
-    summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+    specialty: "Ph.D Candidate",
+    summary: `Howdy! I'm a mechanical engineer and scientist at the University of Michigan.
+    I specialize in biomechanics, human-robot interaction, exoskeleton control, and experimental design.`,
+    email: "nundinir@umich.edu",
   },
   experience: [
-    {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
-      summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
-      ],
-    },
-    {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
-      summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
-      ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
-    },
+    //   {
+    //     company: "Neurobionics Lab",
+    //     position: "Characterizing Value Landscapes for Exoskeleton Assistance",
+    //     startDate: "July 2024",
+    //     endDate: "current",
+    //     summary: [
+    //       "Designed a study to quantify how perceived value of exoskeleton assistance changes with assistance attributes (e.g., peak torque).",
+    //       "Developed a mid-level torque controller",
+    //       "Designed a 3D printed a custom thermal enclosure to mitigate motor overheating.",
+    //       "Currently recruiting participants, finalizing experimental protocols, and characterizing device transmission using a dynamometer.",
+    //     ],
+
+    //   },
+    //   {
+    //     company: "Neurobionics Lab",
+    //     position: "What does User Experience of Exoskeleton Assistance Encode?",
+    //     startDate: "July 2022",
+    //     endDate: "current",
+    //     summary: [
+    //       "Formulated a study to investigate how user perception of exoskeleton assistance relates to physical fatigue during use. ",
+    //       "Led an IRB-approved human study with 10 participants; collected physiological data including metabolic rate and limb kinetics/kinematics.",
+    //       "Currently analyzing data using statistical modeling techniques.",
+    //     ],
+    //   },
+    //   {
+    //     company: "Neurobionics Lab",
+    //     position: "Preferred Ankle Stiffness of a Variable-Stiffness Prosthesis Across Five Activities",
+    //     startDate: "May 2023",
+    //     endDate: "June 2024",
+    //     summary: [
+    //       "Assisted in revamping a Variable Stiffness Prosthetic Ankle (VSPA) foot to determine amputee preferred stiffness for various activities, as well as ankle stiffness sensitivity during these activities.",
+    //       "Performed biomechanical data collection and performed psychometric analysis to determine just-noticeable-differences (JND) in stiffness.",
+    //       "Designed and 3D printed wearable housing for electronic components; created wiring harnesses for robust electrical interfaces.",
+    //     ],
+    //   },
+    //   {
+    //     company: "Neurobionics Lab",
+    //     position: "A Sensitivity Analysis of an Economic Value Metric for Quantifying the Success of Lower-Limb Exoskeletons and their Assistance",
+    //     startDate: "Aug 2021",
+    //     endDate: "July 2022",
+    //     summary: [
+    //       "Designed a study to investigate how virtual robotic agents influence perceived value of exoskeleton assistance.",
+    //       "Developed agents that simulate human fatigue behavior to estimate user perceptions.",
+    //       "Led an IRB-approved study with 14 participants; conducted statistical modeling and data analysis.",
+    //     ]
+    //   },
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
+      name: "Characterizing Value Landscapes for Exoskeleton Assistance",
+      summary: "How does user value of exoskeleton assistance change with assistance attributes (e.g., peak torque)?",
+      // linkPreview: "/",
+      linkSource: "https://github.com/neurobionics/Exoboot-Controller-VAS",
       image: "/spotifu.png",
     },
     {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
+      name: "What does User Experience of Exoskeleton Assistance Encode?",
+      summary: "How does user perception of exoskeleton assistance relate to physical fatigue during use. ",
+      // linkPreview: "/",
+      // linkSource: "https://github.com/immois/astro-zen",
       image: "/shopify-clon.png",
     },
     {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
+      name: "Preferred Ankle Stiffness of a Variable-Stiffness Prosthesis Across 5 Activities",
+      summary: "What ankle stiffness do amputees prefer for various activities?",
       linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
+      // linkSource: "https://github.com/immois/astro-zen",
       image: "/clone-ig.png",
     },
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
-
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+    Hi there! I'm a PhD candidate in Mechanical Engineering at the University of Michigan,
+    where I study how to develop exoskeletons that are both biomechanically effective and meaningful to wear.
+    My work combines human-subject studies, control development, and behavioral metrics to quantify
+    and integrate user experience directly into exoskeleton design.
     `,
     image: "/alejandro-big.jpg",
   },
